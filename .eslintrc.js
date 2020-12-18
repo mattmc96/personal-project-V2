b3ef266ca5689fb2d2a1a32e18ac0eac43dfefc5
Matthew Mccarthy <matthewmc96@gmail.com>
2020-12-17 23:04:42 -0600

    chores: boiler plate for --typescript

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+
+  },
+  settings: {
+    react: {
+    version: 'detect',
+},
+},
+}

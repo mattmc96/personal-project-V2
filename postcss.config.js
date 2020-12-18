3a4727be6452953267ef796993bf891a4ed8c8dd
Matthew Mccarthy <matthewmc96@gmail.com>
2020-12-18 14:07:43 -0600

    chores: cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,17 @@
 module.exports = {
     plugins: [
-      require('tailwindcss'),
-        require('autoprefixer')
-      cssnano({
-        preset: 'default'
-      }),
-      purgecss({
-        content: ['./layouts/**/*.html', './src/']
-      })
+        require('tailwindcss'),
+        require('autoprefixer'),
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        require('@fullhuman/postcss-purgecss')({
+            content: ['./src/**/*.js', './src/**/*.tsx', './src/**/*.ts', './public/index.html'],
+            defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        }),
+        cssnano({
+            preset: 'default',
+        }),
+        purgecss({
+            content: ['./layouts/**/*.html', './src/'],
+        }),
     ],
-
-
 };

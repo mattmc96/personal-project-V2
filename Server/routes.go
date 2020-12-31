cc33204cbf2991556600e124457df55c8acffe26
Matthew Mccarthy <matthewmc96@gmail.com>
2020-12-30 22:23:13 -0600

    chores: default

--- Server/routes.go
+++ Server/routes.go
@@ -9,7 +9,21 @@ import (
 )
 
 func setStaticFolder(route *mux.Router) {
-    fs := http.FileServer(http.Dir('./public/"))
+    fs := http.FileServer(http.Dir("./public/"))
+    route.PathPrefix("/public/").Handler(http.StripPrefix("/public/", fs))
 }
 
-route.HandleFunc("/isUsernameAvailable/{username}", handlers.IsUsernameAvailable)
+// AddApproutes will add the routes for the application
+func AddApproutes(route *mux.Router) {
+    log.Println("Loading Routes...")
+
+    setStaticFolder(route)
+
+    route.HandleFunc("/", RenderHome)
+
+    route.HandleFunc("/isUsernameAvailable/{username}", handlers.IsUsernameAvailable)
+
+    route.HandleFunc("/users/{name}", GetUsers).Methods("Get")
+
+    log.Println("routes are loaded")
+}

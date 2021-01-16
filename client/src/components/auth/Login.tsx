4d6ee42a8f91fd541fe28f48f335b2ae53c2b9ec
Matthew Mccarthy <matthewmc96@gmail.com>
2021-01-15 21:54:00 -0600

    wip

--- client/src/components/auth/Login.tsx
+++ client/src/components/auth/Login.tsx
@@ -3,34 +3,35 @@ import React from 'react';
 const Landing: React.FC = () => {
     const loginRef = React.useRef<HTMLFormElement>(null);
 
-    // const loginSubmitHandler = (e: React.FormEvent) => {
-    //     e.preventDefault();
-    //     const target = e.target as typeof e.target & {
-    //         email: { value: string };
-    //         password: { value: string };
-    //     };
-    //     // const email = target.email.value;
-    //     // const password = target.password.value;
-    // };
+    const loginSubmitHandler = (e: React.FormEvent) => {
+        e.preventDefault();
+        const target = e.target as typeof e.target & {
+            email: { value: string };
+            password: { value: string };
+        };
+        // const email = target.email.value;
+        // const password = target.password.value;
+    };
 
     return (
-        <form ref={loginRef}
+        <form
+            ref={loginRef}
             // onSubmit={loginSubmitHandler}
         >
             <div>
                 <label>
                     Email:
-                    <input type="email" name="email"/>
+                    <input type="email" name="email" />
                 </label>
             </div>
             <div>
                 <label>
                     Password:
-                    <input type="password" name="password"/>
+                    <input type="password" name="password" />
                 </label>
             </div>
             <div>
-                <input type="submit" value="login"/>
+                <input type="submit" value="login" />
             </div>
         </form>
     );

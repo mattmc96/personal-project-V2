3f2be6435161c6c7bb1c2e0683984c3a792376bf
ray dominguez <matthewmc96@gmail.com>
2021-01-27 17:48:29 -0600

    wip

---
+++ client/src/redux/actions/userAction.ts
@@ -0,0 +1,14 @@
+import {
+    SET_USER,
+    SET_ERRORS,
+    LOADING_UI,
+    CLEAR_ERRORS,
+    SET_AUTHENTICATED,
+    SET_UNAUTHENTICATED,
+    LOADING_USER,
+} from '../types';
+import axios from 'axios';
+
+export const loginUser = (userData: any, history: any) => (dispatch: any) => {
+    dispatch({ type: LOADING_UI });
+};
